8434ea0ab02baa7d1d8df90c4c438efab1219768
shizhihuaxu <389312733@qq.com>
2019-04-10 15:06:18 +0800

    add palindrome string judgement methods

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 /**
- * 深拷贝对象
+ * 深拷贝对象、数组
  * @author shizhihuaxu 2019-04-08
  * @param  {Object} obj 原对象
  * @return {Object}     新对象
@@ -18,4 +18,32 @@ export const deepClone = (obj) => {
  */
 export const trim = (str) => {
     return (str || '').replace(/^(\s|00A0)+|(\s|00A0)+$/g, '')
+}
+
+/**
+ * 判断字符串是否是回文
+ * @author shizhihuaxu 2019-04-10
+ * @param  {String} str [description]
+ * @return {Boolean}    [description]
+ */
+export const isPalindrome = (str) => {
+    let left = 0
+    let right = str.length - 1
+
+    while (left < right) {
+        if (str[left] !== str[right]) {
+            return false
+        }
+        left++
+        right--
+    }
+
+    return true
+}
+// method 2
+export const isPalindrome1 = (str) => {
+    let arr = str.split('')
+    if (arr.reverse().join('') === str) return true
+
+    return false
 }
